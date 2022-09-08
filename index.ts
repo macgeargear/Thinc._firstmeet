aaa3c62491af451062c8ace2d72c9ea8691f3027
macgeargear <macgeargear@gmail.com>
2022-09-08 18:00:55 +0700

    -a

--- index.ts
+++ index.ts
@@ -1,1 +1,17 @@
-console.log("Gatang dick head");
+//console.log("Gatang dick head");
+
+interface Person {
+  name: string;
+  age: number;
+  cute: boolean;
+}
+
+const person: Person = {
+  name: "Gear",
+  age: 18,
+  cute: true,
+};
+
+console.log(person.name);
+console.log(person.age);
+console.log(person.cute);
